64927b728b60cfe99bfa7a482e65b881d363ae22
Rawi Daniel <rawidaniel96@gmail.com>
2023-02-14 16:15:08 +0300

    fix bug

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -12,7 +12,8 @@ def rotate_2d_matrix(matrix):
     matrix: list[list[int]]
         2d matrix
     """
+    n = len(matrix)
     copy_matrix = [row[:] for row in matrix]
-    for i in range(0, 3):
-        for j in range(2, -1, -1):
-            matrix[j][2-i] = copy_matrix[i][j]
+    for i in range(0, n):
+        for j in range(n - 1, -1, -1):
+            matrix[j][n - 1 -i] = copy_matrix[i][j]
