8dc449106718b1ed311802fc142ddd1f343b23bf
Rawi Daniel <rawidaniel96@gmail.com>
2023-02-26 20:20:38 +0300

    fix error

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
-'''solves the making change problem'''
+"""
+Given a pile of coins of different values, determine the fewest
+number of coins needed to meet a given amount total.
+"""
 
 
 def makeChange(coins, total):
-    if total <= 0:
+    """
+    Given a pile of coins of different values, determine the fewest
+    number of coins needed to meet a given amount total.
+    """
+    sum = 0
+    if (total <= 0):
         return 0
-    dp = [total + 1] * (total + 1)
-    dp[0] = 0
-
-    for a in range(1, total + 1):
-        for coin in coins:
-            if a - coin >= 0:
-                dp[a] = min(dp[a], 1 + dp[a - coin])
-
-    if dp[total] != (total + 1):
-        return dp[total]
-    return -1
+    coins.sort(reverse=True)
+    for i in coins:
+        if (total < i):
+            pass
+        q, r = divmod(total, i)
+        total = r
+        sum += q
+    if (total != 0):
+        return -1
+    return sum
